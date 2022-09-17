d0ebf8dabc8f875fa47cab6f747c6334e4aa7539
nabilk11 <khan.nabiln11.com>
2022-09-17 11:45:32 -0400

    added navbar component with basic styling, and fontawesome social icons with cdn

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './navbar.css'
+
+export const Navbar = () => {
+  return <div className="top">
+<div className="topLeft">
+<i class="fa-brands fa-square-facebook"></i>
+<i class="fa-brands fa-square-twitter"></i>
+<i class="fa-brands fa-square-instagram"></i>
+</div> 
+<div className="topCenter">c</div> 
+<div className="topRight">r</div> 
+  </div>;
+};
